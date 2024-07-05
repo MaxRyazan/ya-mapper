5b28b954b4e7619ff5833b035568d3b0bd828682
MaxRyazan <no4ta87@mail.ru>
2024-07-05 23:10:49 +0300

    add DFlex.vue add DText.vue

--- src/modules/routes/views/cards/BusStationsCard.vue
+++ src/modules/routes/views/cards/BusStationsCard.vue
@@ -16,10 +16,16 @@ defineEmits<{
 <template>
 <d-flex type="column" align="start">
     <d-text @click="$emit('showStation', {name: name, direction: props.direction})"
+            cursor="pointer"
+            class="card__text"
             v-for="name in stations" :key="name">{{name}}</d-text>
 </d-flex>
 </template>
 
 <style scoped>
-
+.card__text {
+    &:hover {
+        color: blue;
+    }
+}
 </style>
