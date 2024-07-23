d80570712220a17f5ff7e9cf551d0fc70464beee
MaxRyazan <no4ta87@mail.ru>
2024-07-23 23:10:20 +0300

    change subNav and routes

---
+++ src/modules/dispatch/views/hash/dispatch/HashLinear.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+</div>
+</template>
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+const route = useRoute()
+
+const currentRoute = ref()
+
+onMounted(() => {
+    currentRoute.value = route.query.route
+})
+</script>
+
+
+<style scoped>
+
+</style>
--- src/modules/dispatch/views/tab/TabMonitor.vue
+++ src/modules/dispatch/views/tab/TabMonitor.vue
@@ -5,21 +5,37 @@
                  :pagination="{defaultPageSize: 14, showSizeChanger: true, pageSizeOptions: ['10', '14', '20', '50']}"
                  :columns="dispatchColumn"
                  :data-source="dataSource">
-            <template #bodyCell="{column}">
+            <template #bodyCell="{column, record}">
                 <d-flex v-if="column.key === 'actions'" justify="center">
-                    <router-link :to="{query: {tab: 'monitor'}, hash: '#map'}">карта</router-link>
-                    <router-link :to="{query: {tab: 'monitor'}, hash: '#linear'}">линейный</router-link>
+                    <router-link :to="{query: {tab: 'monitor', route: record.route}, hash: '#map'}">карта</router-link>
+                    <router-link :to="{query: {tab: 'monitor', route: record.route}, hash: '#linear'}">линейный</router-link>
                 </d-flex>
             </template>
         </a-table>
+        <component :is="currentHashComponent" />
     </div>
 </template>
 <script setup lang="ts">
 import {useRoute} from "vue-router";
 import {dispatchColumn} from "@/modules/dispatch/constants";
+import {shallowRef, watch} from "vue";
+import type {Component} from "vue";
+import HashMap from "@/modules/dispatch/views/hash/dispatch/HashMap.vue";
+import HashLinear from "@/modules/dispatch/views/hash/dispatch/HashLinear.vue";
 
 const route = useRoute()
+const currentHashComponent = shallowRef<Component | null>(null)
 
+watch(() => route.hash, () => {
+    if(route.hash) {
+        currentHashComponent.value = componentMap.get(route.hash)
+    }
+})
+
+const componentMap = new Map([
+    ['#map', HashMap],
+    ['#linear', HashLinear],
+])
 
 const dataSource = [
     {
