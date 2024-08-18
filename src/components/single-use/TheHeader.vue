e03f059aa1ec7751ebcf53079ab2cab95fe41f27
MaxRyazan <no4ta87@mail.ru>
2024-08-18 11:36:02 +0300

    add phone to authorization

--- src/components/single-use/TheHeader.vue
+++ src/components/single-use/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <d-flex class="header">
-        <div style="width: calc(100% - 240px); padding-left: 40px; overflow-x: auto" id="header-id">
+        <div style="width: calc(100% - 240px); padding-left: 28px; overflow-x: auto" id="header-id">
             <slot />
         </div>
         <user-info />
